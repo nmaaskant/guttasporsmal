39219761c25c06b27332ff2714eddf22cb5e13f9
nma044 <nathanielm@lyse.net>
2022-06-18 12:36:45 +0200

    startskjerm test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,40 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import Snusboks from './components/Snusboks';
 import SnusboksLoader from './components/SnusboksLoader';
+import Snusinfo from './components/Snusinfo';
 
 function App() {
 
   const [loading, setLoading] = useState(false);
+
+  const tekst = <h3>Snusboksleken går ut på snusboks
+  blir kastet til den personen som påstanden passer best til. 
+  Personen som mottar snusboksen drikker 3 slurker og kaster videre. 
+  Kan også inkludere splash, hvor personene som sitter ved siden av
+  den som får boksen også må drikke et visst antall slurker</h3>;
+
+  const snus = () => {
+    setMode( loading ? <SnusboksLoader /> : <Snusboks round="0" />)
+  }
+
+  const info = () => {
+    setMode(tekst)
+  }
+
+    
+const start = 
+  <div className='start'>
+    <div className='buttons'>
+      <button onClick={snus} className="snusButton">Snusboksleken</button>
+      <button onClick={info} className="info">i</button>
+      <button className="disabled">Flere spill kommer snart!</button>
+    </div>
+  </div>;
+
+  const [mode, setMode] = useState(start);
+
+  const backButton = <button onClick={setMode(start)}>Tilbake</button>;
+
 
 useEffect(() => {
     setLoading(true)
@@ -16,7 +46,7 @@ useEffect(() => {
 
   return (
     <div className='App'>
-      { loading ? <SnusboksLoader /> : <Snusboks round="0" />}
+      {mode}
     </div>
  );
 }
---
+++ src/components/Snusinfo.js
@@ -0,0 +1,16 @@
+function Snusinfo(){
+    const tekst = <h3>Snusboksleken går ut på snusboks
+        blir kastet til den personen som påstanden passer best til. 
+        Personen som mottar snusboksen drikker 3 slurker og kaster videre. 
+        Kan også inkludere splash, hvor personene som sitter ved siden av
+        den som får boksen også må drikke et visst antall slurker
+      </h3>;
+    return(
+        
+        tekst
+
+    )
+
+}
+
+export default Snusinfo;

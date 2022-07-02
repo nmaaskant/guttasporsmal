e7c7c4ee40bd59dd3513ea6d192e8dcc54895ec5
nma044 <nathanielm@lyse.net>
2022-07-02 16:49:39 +0200

    Updates

---
+++ precache-manifest.2a28942d9d9b1ae41933ad3f6377f1f2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d044794138e36ae470",
+    "url": "/guttasporsmal/static/css/main.76325cb1.chunk.css"
+  },
+  {
+    "revision": "d6d044794138e36ae470",
+    "url": "/guttasporsmal/static/js/main.d6d04479.chunk.js"
+  },
+  {
+    "revision": "e09cd7cc5b8cebfd4d75",
+    "url": "/guttasporsmal/static/js/1.e09cd7cc.chunk.js"
+  },
+  {
+    "revision": "f6321334fb9d1005cfee",
+    "url": "/guttasporsmal/static/js/runtime~main.f6321334.js"
+  },
+  {
+    "revision": "bdc80134da552b01a7e51f658f3b5609",
+    "url": "/guttasporsmal/static/media/backflip-crackhead.bdc80134.gif"
+  },
+  {
+    "revision": "2b71a57f4317c8974866fd01cda9a760",
+    "url": "/guttasporsmal/static/media/beervomit2.2b71a57f.gif"
+  },
+  {
+    "revision": "40715822ca4005cb3f5b06146dcff437",
+    "url": "/guttasporsmal/static/media/trailer.40715822.gif"
+  },
+  {
+    "revision": "8e0160ba264cc427a3d8433ad765e1a9",
+    "url": "/guttasporsmal/static/media/chug.8e0160ba.gif"
+  },
+  {
+    "revision": "c57e731b1ef4d697ed0228ebc06bb24d",
+    "url": "/guttasporsmal/static/media/beervomit.c57e731b.gif"
+  },
+  {
+    "revision": "a9e748c8abfd91685d21e2a2c5fba7b2",
+    "url": "/guttasporsmal/static/media/spongebob.a9e748c8.gif"
+  },
+  {
+    "revision": "e954a5e8559cf4c5a0587cab5e76964e",
+    "url": "/guttasporsmal/static/media/rave.e954a5e8.gif"
+  },
+  {
+    "revision": "b13f1b3e51eda31e0e76b49b3ce9d381",
+    "url": "/guttasporsmal/static/media/cat.b13f1b3e.gif"
+  },
+  {
+    "revision": "96904bfd3ba5318b3ac472f8f2a8df0c",
+    "url": "/guttasporsmal/static/media/street-rave.96904bfd.gif"
+  },
+  {
+    "revision": "6995725983ba33aa7433005ca73d00f9",
+    "url": "/guttasporsmal/static/media/fail.69957259.gif"
+  },
+  {
+    "revision": "f0ebe5749479425eca636f75c1b1400b",
+    "url": "/guttasporsmal/static/media/dancing-rave.f0ebe574.gif"
+  },
+  {
+    "revision": "3ba7ea81c8f8a144021d11f32e4071bf",
+    "url": "/guttasporsmal/index.html"
+  }
+];
